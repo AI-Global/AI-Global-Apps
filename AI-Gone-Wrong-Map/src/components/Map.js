cd2446de2912fda9f3f919074e9d662663060b85
Shrivu Shankar <shrivu1122@gmail.com>
2020-09-15 12:46:48 -0500

    Add Time Slider (#57)
    * componetize
    
    * added ant design comps
    
    * redid filtering logic and added slider
    
    * enabled year slider

--- AI-Gone-Wrong-Map/src/components/Map.js
+++ AI-Gone-Wrong-Map/src/components/Map.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import mapboxgl from 'mapbox-gl';
 import db from '../data/db';
+import { Slider } from 'antd';
 
 // renderPulse - Function to render dot -regardless if bad/good
 function renderPulse(map, context, size, offset, domain, colors) {
   let t = 0.8;
   let radius = (size / 2) * 0.3;
   context.clearRect(0, 0, size, size);
-  if (!domainToVisable[domain]) {
-    return;
-  }
   context.beginPath();
   context.arc(size / 2, size / 2, radius, 0, Math.PI * 2);
   context.fillStyle = `rgba(${colors[1][0]}, ${colors[1][1]}, ${colors[1][2]}, 1)`;
@@ -23,14 +21,11 @@ function renderPulse(map, context, size, offset, domain, colors) {
 // renderPulseGood - function for drawing a star
 // think of size as the plot, "can only be in this space"
 // always need map, context,size,offset
-function renderPulseGood(map, context, size, offset, domain, colors, is_good) {
+function renderPulseGood(map, context, size, offset, domain, colors, isGood) {
   let alpha = (2 * Math.PI) / 10;
   let radius = size * 0.2;
   let starXY = [size / 2, size / 2];
   context.clearRect(0, 0, size, size);
-  if (!is_goodToVisable[is_good]) {
-    return;
-  }
   context.beginPath();
   // Star Formula
   for (let i = 11; i !== 0; i--) {
@@ -50,11 +45,11 @@ function renderPulseGood(map, context, size, offset, domain, colors, is_good) {
 }
 
 // Define variables
-let domainsSelected = ['Society', 'Law Enforcement', 'Business'];
+let initialDomainsSelected = ['Society', 'Law Enforcement', 'Business'];
 
 let domains = [...new Set(db.map((item) => item.domain.trim()))];
 
-let goodnessSelected = ['Right', 'Wrong'];
+let initialGoodnessSelected = ['Right', 'Wrong'];
 
 let goodness = [...new Set(db.map((item) => item.is_good.trim()))];
 
@@ -76,26 +71,29 @@ let domainColors = [
 ];
 
 let domainToColors = {};
-let domainToVisable = {};
-let domainsToMarkers = {};
-//Initilaized for colors
-let is_goodToVisable = {};
-let is_goodToMarkers = {};
 
 for (let dIdx in domains) {
   let [r, g, b] = domainColors[dIdx];
   domainToColors[domains[dIdx]] = [[r, g, b], [r + 50, g + 50, b + 50], `rgba(${r}, ${g}, ${b}, 1)`];
-  domainToVisable[domains[dIdx]] = domainsSelected.includes(domains[dIdx]);
-  domainsToMarkers[domains[dIdx]] = [];
 }
 
-for (let gIdx in goodness) {
-  is_goodToVisable[goodness[gIdx]] = goodnessSelected.includes(goodness[gIdx]);
-  is_goodToMarkers[goodness[gIdx]] = [];
+let yearMarks = {};
+let startYear = 2010;
+let endYear = 2020;
+for (let yr = startYear; yr <= endYear; yr += 1) {
+  yearMarks[yr] = '' + yr;
 }
+
+// Clean input data inplace
+db.map((item, i) => {
+  item.domain = item.domain.trim();
+  item.isGood = item.is_good.trim();
+  item.year = parseInt(item.year);
+  item.id = i;
+});
 
 let eventToFeatureJSON = (event) => {
-  let { title, issue, lat, lng, link, domain, city, state, country, is_good } = event;
+  let { title, issue, lat, lng, link, domain, city, state, country, isGood } = event;
   let loc = '';
   if (city && state && country) {
     loc = `${city} ${state}, ${country}`;
@@ -114,7 +112,7 @@ let eventToFeatureJSON = (event) => {
       description: issue,
       location: loc,
       link: link,
-      is_good: is_good,
+      isGood: isGood,
     },
     geometry: {
       type: 'Point',
@@ -129,8 +127,9 @@ class Map extends React.Component {
     this.state = {
       width: window.innerWidth,
       height: window.innerHeight,
-      selected: domainsSelected,
-      selectedgood: goodnessSelected,
+      selected: initialDomainsSelected,
+      selectedGood: initialGoodnessSelected,
+      selectedYears: Object.keys(yearMarks),
       lng: 5,
       lat: 34,
       zoom: 1.7,
@@ -163,7 +162,7 @@ class Map extends React.Component {
         let offset = Math.random() * 1000;
         let colors = domainToColors[item.domain];
         let popUpHTML = `<h3>${marker.properties.title}</h3>
-          <p><i>${marker.properties.category}</i><p><i>${marker.properties.is_good}</i><br/>${marker.properties.location}</p>
+          <p><i>${marker.properties.category}</i><p><i>${marker.properties.isGood}</i><br/>${marker.properties.location}</p>
           <p>${marker.properties.description}</p>`;
         if (marker.properties.link) {
           popUpHTML += `<a target="_blank" href="${marker.properties.link}">More Info</a>`;
@@ -178,21 +177,16 @@ class Map extends React.Component {
         let mrkr = new mapboxgl.Marker(canvas)
           .setLngLat([x, y])
           .setPopup(new mapboxgl.Popup({ offset: 25 }).setHTML(popUpHTML));
-        domainsToMarkers[item.domain.trim()].push(mrkr);
-        if (domainsSelected.includes(item.domain.trim())) {
-          mrkr.addTo(map);
-        }
-
+        item.mrkr = mrkr;
         // For filter to work! (Functionality on legend)
-        is_goodToMarkers[item.is_good.trim()].push(mrkr);
-        if (goodnessSelected.includes(item.is_good.trim())) {
+        if (initialGoodnessSelected.includes(item.isGood) && initialDomainsSelected.includes(item.domain)) {
           mrkr.addTo(map);
         }
         let markerRender = () => {
           requestAnimationFrame(markerRender);
-          // If the value in is_good ever changes, need to edit here
-          if (item.is_good === 'Right') {
-            renderPulseGood(map, context, 50, offset, item.domain, colors, item.is_good);
+          // If the value in isGood ever changes, need to edit here
+          if (item.isGood === 'Right') {
+            renderPulseGood(map, context, 50, offset, item.domain, colors, item.isGood);
           } else {
             renderPulse(map, context, 50, offset, item.domain, colors);
           }
@@ -205,55 +199,55 @@ class Map extends React.Component {
     });
   }
 
+  updateMarkersShown(selected, selectedGood, selectedYears) {
+    for (let item of db) {
+      if (selectedGood.includes(item.isGood) && selected.includes(item.domain) && selectedYears.includes(item.year)) {
+        item.mrkr.addTo(window.map);
+      } else {
+        item.mrkr.remove();
+      }
+    }
+  }
+
   onClickDomain(domain) {
     this.setState((state) => {
       if (state.selected.includes(domain)) {
-        domainToVisable[domain] = false;
         state.selected = state.selected.filter((x) => x !== domain);
-        for (let dm in domainToVisable) {
-          if (!domainToVisable[dm]) {
-            for (let marker of domainsToMarkers[dm]) {
-              marker.remove();
-            }
-          }
-        }
       } else {
-        domainToVisable[domain] = true;
         state.selected.push(domain);
-        for (let marker of domainsToMarkers[domain]) {
-          marker.addTo(window.map);
-        }
       }
+      this.updateMarkersShown(state.selected, state.selectedGood, state.selectedYears);
       return state;
     });
   }
 
   //onClickGoodness - Function to get legend filtering to work
-  onClickGoodness(is_good) {
+  onClickGoodness(isGood) {
     this.setState((state) => {
-      if (state.selectedgood.includes(is_good)) {
-        is_goodToVisable[is_good] = false;
-        state.selectedgood = state.selectedgood.filter((x) => x !== is_good);
-        for (let ig in is_goodToVisable) {
-          if (!is_goodToVisable[ig]) {
-            for (let marker of is_goodToMarkers[ig]) {
-              marker.remove();
-            }
-          }
-        }
+      if (state.selectedGood.includes(isGood)) {
+        state.selectedGood = state.selectedGood.filter((x) => x !== isGood);
       } else {
-        is_goodToVisable[is_good] = true;
-        state.selectedgood.push(is_good);
-        for (let marker of is_goodToMarkers[is_good]) {
-          marker.addTo(window.map);
-        }
+        state.selectedGood.push(isGood);
       }
+      this.updateMarkersShown(state.selected, state.selectedGood, state.selectedYears);
+      return state;
+    });
+  }
+
+  onYearSliderChange(newRange) {
+    let yearSelection = [];
+    for (let i = newRange[0]; i <= newRange[1]; i++) {
+      yearSelection.push(i);
+    }
+    this.setState((state) => {
+      state.selectedYears = yearSelection;
+      this.updateMarkersShown(state.selected, state.selectedGood, state.selectedYears);
       return state;
     });
   }
 
   render() {
-    let { width, height, zoom, selected, selectedgood } = this.state;
+    let { width, height, zoom, selected, selectedGood } = this.state;
     return (
       <div>
         <div className="logo-box">
@@ -261,68 +255,25 @@ class Map extends React.Component {
             <img alt="AI Global Logo" src="/transparent-rect-logo.png" />
           </a>
         </div>
-        <div className="legend-box">
-          <p>Domains</p>
-          {domains.map((domain) => (
-            <div>
-              <input type="checkbox" checked={selected.includes(domain)} onClick={() => this.onClickDomain(domain)} />
-              <div style={{ backgroundColor: domainToColors[domain][2] }} className="color-block"></div> {domain}
-            </div>
-          ))}
-          <br />
-          <p> Where AI Has Gone...</p>
-          {goodness.map((is_good) => (
-            <div>
-              <input
-                type="checkbox"
-                checked={selectedgood.includes(is_good)}
-                onClick={() => this.onClickGoodness(is_good)}
-              />
-              {is_good}
-            </div>
-          ))}
-
-          <br />
-          <div style={{ textAlign: 'center' }}>
-            {/* <a target="_blank" href="https://portal.ai-global.org/dataset/ai-violation-use-cases">View Dataset</a> */}
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://docs.google.com/spreadsheets/d/1hUAGsMGT-tbcboF6zzbtFHowT9k0yKjjy7K8hfbEuG8/edit#gid=0"
-            >
-              View Dataset
-            </a>
-            <br />
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://docs.google.com/forms/d/e/1FAIpQLSeo4ZcT48qYDA3Z4GgRF8TjNLVuHpAvt9I1rVDX87usskLoVQ/viewform"
-            >
-              Submit a use case
-            </a>
-          </div>
+        <Legend
+          selected={selected}
+          selectedGood={selectedGood}
+          onClickDomain={this.onClickDomain.bind(this)}
+          onClickGoodness={this.onClickGoodness.bind(this)}
+        />
+        <TitleBox zoom={zoom} />
+        <InfoBox />
+        <div className="slider-box">
+          <Slider
+            onChange={(v) => this.onYearSliderChange(v)}
+            range
+            marks={yearMarks}
+            min={startYear}
+            max={endYear}
+            step={null}
+            defaultValue={[startYear, endYear]}
+          />
         </div>
-        <div className="title-box">
-          <h1 style={{ margin: 'auto', width: '45%', marginBottom: '20px' }}>Where in the World is AI?</h1>
-          {zoom < 3.55 && (
-            <h5 style={{ margin: 'auto', width: '40%' }}>
-              Everyone is talking about AI, but how and where is it actually being used? Since our mission is to ensure
-              AI is protecting us instead of harming us, we’ve mapped out some cases where AI is being used well, and
-              times where it has gone wrong. Cases are aggregated by AI Global and Charlie Pownall/CPC & Associates{' '}
-              <a href="https://docs.google.com/spreadsheets/d/1Bn55B4xz21-_Rgdr8BBb2lt0n_4rzLGxFADMlVW0PYI/edit#gid=364376814">
-                (AI & Algorithmic Controversy Repository)
-              </a>
-            </h5>
-          )}
-        </div>
-        <a target="_blank" rel="noopener noreferrer" href="https://oproma.github.io/rai-trustindex/">
-          <button className="call-to-action-button" type="submit">
-            These consequences can be avoided by improving the design of AI.
-            <br />
-            <br />
-            Check your AI with our <em>Design Assistant</em>
-          </button>
-        </a>
         <div
           id="#map"
           ref={(elem) => (this.mapContainer = elem)}
@@ -331,6 +282,77 @@ class Map extends React.Component {
       </div>
     );
   }
+}
+
+function Legend({ selected, selectedGood, onClickDomain, onClickGoodness }) {
+  return (
+    <div className="legend-box">
+      <p>Domains</p>
+      {domains.map((domain) => (
+        <div>
+          <input type="checkbox" checked={selected.includes(domain)} onClick={() => onClickDomain(domain)} />
+          <div style={{ backgroundColor: domainToColors[domain][2] }} className="color-block"></div> {domain}
+        </div>
+      ))}
+      <br />
+      <p> Where AI Has Gone...</p>
+      {goodness.map((isGood) => (
+        <div>
+          <input type="checkbox" checked={selectedGood.includes(isGood)} onClick={() => onClickGoodness(isGood)} />
+          {isGood}
+        </div>
+      ))}
+      <br />
+      <div style={{ textAlign: 'center' }}>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://docs.google.com/spreadsheets/d/1hUAGsMGT-tbcboF6zzbtFHowT9k0yKjjy7K8hfbEuG8/edit#gid=0"
+        >
+          View Dataset
+        </a>
+        <br />
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://docs.google.com/forms/d/e/1FAIpQLSeo4ZcT48qYDA3Z4GgRF8TjNLVuHpAvt9I1rVDX87usskLoVQ/viewform"
+        >
+          Submit a use case
+        </a>
+      </div>
+    </div>
+  );
+}
+
+function TitleBox({ zoom }) {
+  return (
+    <div className="title-box">
+      <h1 style={{ margin: 'auto', width: '45%', marginBottom: '20px' }}>Where in the World is AI?</h1>
+      {zoom < 3.55 && (
+        <h5 style={{ margin: 'auto', width: '40%' }}>
+          Everyone is talking about AI, but how and where is it actually being used? Since our mission is to ensure AI
+          is protecting us instead of harming us, we’ve mapped out some cases where AI is being used well, and times
+          where it has gone wrong. Cases are aggregated by AI Global and Charlie Pownall/CPC & Associates{' '}
+          <a href="https://docs.google.com/spreadsheets/d/1Bn55B4xz21-_Rgdr8BBb2lt0n_4rzLGxFADMlVW0PYI/edit#gid=364376814">
+            (AI & Algorithmic Controversy Repository)
+          </a>
+        </h5>
+      )}
+    </div>
+  );
+}
+
+function InfoBox() {
+  return (
+    <a target="_blank" rel="noopener noreferrer" href="https://oproma.github.io/rai-trustindex/">
+      <button className="call-to-action-button" type="submit">
+        These consequences can be avoided by improving the design of AI.
+        <br />
+        <br />
+        Check your AI with our <em>Design Assistant</em>
+      </button>
+    </a>
+  );
 }
 
 export default Map;
